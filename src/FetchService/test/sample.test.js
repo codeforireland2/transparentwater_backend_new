44aa745efc4fe928f000b4e0a6aaf59cf7e1aae4
Luke Kearney <luke.t.kearney@gmail.com>
2018-05-07 16:19:47 +0100

    test: updating linter

--- src/FetchService/test/sample.test.js
+++ src/FetchService/test/sample.test.js
@@ -1,3 +1,3 @@
-test('sample test', () => { // eslint-disable-line no-undef
+test("sample test", () => { // eslint-disable-line no-undef
     expect(1 + 2).toBe(3); // eslint-disable-line no-undef
 });
