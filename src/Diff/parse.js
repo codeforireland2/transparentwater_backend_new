34307d375b7f162ac56f172570813c71694d69e6
Luke Kearney <luke.t.kearney@gmail.com>
2018-06-04 13:42:01 +0100

    fixed linting

--- src/Diff/parse.js
+++ src/Diff/parse.js
@@ -7,131 +7,86 @@ const util = require('util');
 // at the moment this just returns the events. It assumes data is a single object with
 // no further children
 class TransactionEvents {
-	constructor() {
-		this.updateEvents = [];
-		this.removeEvents = [];
-		this.addEvents = [];
-	}
+  constructor() {
+    this.updateEvents = [];
+    this.removeEvents = [];
+    this.addEvents = [];
+  }
 
-	// we may update this function in future to specify only the fields modified
-	// and their new values
-	addUpdateEvent(key, value) {
-		this.updateEvents.push({
-			key: key,
-			data: value,
-		});
-	}
+  // we may update this function in future to specify only the fields modified
+  // and their new values
+  addUpdateEvent(key, value) {
+    this.updateEvents.push({
+      key: key,
+      data: value
+    });
+  }
 
-	addRemoveEvent(key) {
-		this.removeEvents.push({
-			key: key,
-			data: null,
-		})
-	}
+  addRemoveEvent(key) {
+    this.removeEvents.push({
+      key: key,
+      data: null
+    });
+  }
 
-	addAddEvent(key, value) {
-		this.addEvents.push({
-			key: key,
-			data: value,
-		})
-	}
+  addAddEvent(key, value) {
+    this.addEvents.push({
+      key: key,
+      data: value
+    });
+  }
 
-	get getObject() {
-		return {
-			addEvents: this.addEvents,
-			removeEvents: this.removeEvents,
-			updateEvents: this.updateEvents
-		}
-	}
+  get getObject() {
+    return {
+      addEvents: this.addEvents,
+      removeEvents: this.removeEvents,
+      updateEvents: this.updateEvents
+    };
+  }
 }
 
-const createKeyedTree = function(jsonData, parseFunction) {
-	// cycle through the JSON data and fetch the keyed field
-	return parseFunction(jsonData);
+function createKeyedTree(jsonData, parseFunction) {
+  // cycle through the JSON data and fetch the keyed field
+  return parseFunction(jsonData);
 }
 
-const getUniqueKeys = function(a, b) {
-	let seen = {};
-	return a.concat(b).filter(function(k) {
-		if (seen[k]) {
-			return false;
-		}
+function getUniqueKeys(a, b) {
+  const seen = {};
+  return a.concat(b).filter((k) => {
+    if (seen[k]) {
+      return false;
+    }
 
-		seen[k] = true;
-		return true;
-	});
-
+    seen[k] = true;
+    return true;
+  });
 }
 
-const type = function(val) {
-	if (val == null) {
-		return 'null';
-	}
+function createTransactionDiff(oldData, newData) {
+  const transaction = new TransactionEvents();
+  const keys = getUniqueKeys(Object.keys(oldData), Object.keys(newData));
 
-	if (val instanceof Array) {
-		return 'array';
-	}
-	return typeof val; 
-}
+  keys.forEach((k) => {
+    // removed case
+    if (oldData[k] && !newData[k]) {
+      transaction.addRemoveEvent(k);
+    }
 
-// const deepEqual = function(a, b) {
-// 	if (type(a) != type(b)) {
-// 		throw 'Types not equal' + type(a) + 'is not' + type(b);
-// 	}
+    if (!oldData[k] && newData[k]) {
+      transaction.addAddEvent(k, newData[k]);
+    }
 
-// 	if (type(a) == 'array') {
-// 		a.sort();
-// 		b.sort();
-// 		return a.reduce(function(acc, val) {
-// 			console.log("checking " + val)
-// 			return deepEqual(a[val], b[val])
-// 		});
-// 	}
-
-// 	if (type(a) == 'object') {
-// 		return Object.keys(a).reduce(function(acc, val) {
-// 			return deepEqual(a[val], b[val])
-// 		});
-// 	}
-
-// 	return a === b;
-// }
-
-const createTransactionDiff = function(oldData, newData) {
-	const transaction = new TransactionEvents();
-	const keys = getUniqueKeys(Object.keys(oldData), Object.keys(newData));
-
-	keys.forEach(function(k) {
-	  const inOld = oldData[k] ? true : false;
-	  const inNew = newData[k] ? true : false;
-	  // removed case
-	  if (inOld && !inNew) {
-	  	transaction.addRemoveEvent(k);
-	  };
-
-	  if (!inOld && inNew) {
-	  	transaction.addAddEvent(k, newData[k]);
-	  };
-
-	  // at this point there's a potential that the values differ
-	  if (inOld && inNew) {
-	    if (!util.isDeepStrictEqual(oldData[k], newData[k])) {
-	    	transaction.addUpdateEvent(k, newData[k]);
-	    }
-	  }
-	});
-	console.log(transaction.getObject);
-	return transaction.getObject;
-
-	// get keys from old
-	// get keys from new
-	// for each key check if in other tree
-	// if in old tree and not in new, then add a remove event for that key
-	// if not in old tree and is in new, then 
+    // at this point there's a potential that the values differ
+    if (oldData[k] && newData[k]) {
+      if (!util.isDeepStrictEqual(oldData[k], newData[k])) {
+        transaction.addUpdateEvent(k, newData[k]);
+      }
+    }
+  });
+  return transaction.getObject;
 }
 
 module.exports = {
-	keyedTree: createKeyedTree,
-	getDiff: createTransactionDiff,
-}
-
+  keyedTree: createKeyedTree,
+  getDiff: createTransactionDiff
+};
--- src/Diff/test/parse.test.js
+++ src/Diff/test/parse.test.js
@@ -25,7 +25,7 @@ test('Diff: create keyed JSON tree', () => { // eslint-disable-line no-undef
   }];
 
   const expected = {
-    'ABC1234': {
+    ABC1234: {
       OBJECTID: 1,
       WORKTYP: null,
       TITLE: 'Essential Maintenance Works - Dublin',
@@ -46,14 +46,16 @@ test('Diff: create keyed JSON tree', () => { // eslint-disable-line no-undef
     }
   };
 
-  const parseFunction = function(input) {
-    let newObj = {};
-    input.forEach(function(i) {
+  function parseFunction(parseInput) {
+    const newObj = {};
+    parseInput.forEach((i) => {
       newObj[i.REFERENCENUM] = i;
     });
     return newObj;
   }
-  expect(parse.keyedTree(input, parseFunction)).toEqual(expected);
+
+  expect(parse.keyedTree(input, parseFunction)) // eslint-disable-line no-undef
+    .toEqual(expected);
 });
 
 test('Diff: create transaction', () => { // eslint-disable-line no-undef
@@ -61,43 +63,44 @@ test('Diff: create transaction', () => { // eslint-disable-line no-undef
   // JOB4 to be added
   // JOB2 to be modified
   // JOB3 to not show up at all
-  expectedTransaction = {
+  const expectedTransaction = {
     addEvents: [{
-      key: "JOB4",
+      key: 'JOB4',
       data: {
-        "TITLE": "Essential Maintenance Works - Leitrim",
-        "COUNTY": "Leitrim",
-        "REFERENCENUM": "JOB4",
-        "LAT": 53.926507006511,
-        "LONG": -8.0085297962951,
-        "NOTICETYPE": [
-          "WATEROUTAGE"
-        ] 
-      } 
+        TITLE: 'Essential Maintenance Works - Leitrim',
+        COUNTY: 'Leitrim',
+        REFERENCENUM: 'JOB4',
+        LAT: 53.926507006511,
+        LONG: -8.0085297962951,
+        NOTICETYPE: [
+          'WATEROUTAGE'
+        ]
+      }
     }],
     removeEvents: [{
-      key: "JOB1",
+      key: 'JOB1',
       data: null
     }],
     updateEvents: [{
-      key: "JOB2",
+      key: 'JOB2',
       data: {
-        "TITLE": "Essential Maintenance Works - Waterford",
-        "COUNTY": "Waterford",
-        "REFERENCENUM": "JOB2",
-        "PROJECT": null,
-        "LAT": 56,
-        "LONG": 78,
-        "NOTICETYPE": [
-          "WATEROUTAGE", "BOILNOTICE"
+        TITLE: 'Essential Maintenance Works - Waterford',
+        COUNTY: 'Waterford',
+        REFERENCENUM: 'JOB2',
+        PROJECT: null,
+        LAT: 56,
+        LONG: 78,
+        NOTICETYPE: [
+          'WATEROUTAGE', 'BOILNOTICE'
         ]
       }
     }]
-  }
+  };
+
   const oldFile = path.join(__dirname, '../../..', 'test/diff-sample-in-old.json');
   const newFile = path.join(__dirname, '../../..', 'test/diff-sample-in-new.json');
-  let oldData = JSON.parse(fs.readFileSync(oldFile, 'utf8'));
-  let newData = JSON.parse(fs.readFileSync(newFile, 'utf8'));
+  const oldData = JSON.parse(fs.readFileSync(oldFile, 'utf8'));
+  const newData = JSON.parse(fs.readFileSync(newFile, 'utf8'));
   const diffed = parse.getDiff(oldData, newData);
-  expect(diffed).toEqual(expectedTransaction);
+  expect(diffed).toEqual(expectedTransaction); // eslint-disable-line no-undef
 });
